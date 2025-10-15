7094cdcd30b79462a767a35562aa6a3206658962
Ernesto Rios <ernestorios@outlook.com.pe>
2025-10-14 23:13:52 -0500

    Pipeline Gold

--- src/pipelinegold.py
+++ src/pipelinegold.py
@@ -9,22 +9,45 @@ class PipelineGold():
         self.layer = "GOLD"
         
         self.spark = SparkSession.builder.appName("UBIGEO2002-GOLD").master("local[*]").enableHiveSupport().getOrCreate()
+
+        self.df_silver_departamentos2002 = None
+        self.df_silver_provincias2002 = None
+
+        self.df_departamentos_capitales_provincias2002 = None
     
     def extracting(self):
         try:
-            pass
+            extractor = Extract()
+    
+            self.df_silver_departamentos2002 = extractor.dataframe_from_table(self.spark, "silver_departamentos2002")
+            self.df_silver_provincias2002 = extractor.dataframe_from_table(self.spark, "silver_provincias2002")
         except Exception as e:
             print(f"Error extracting data: {e}")
 
     def transforming(self):
         try:
-            pass
+            transformer = Transform()
+    
+            self.df_departamentos_capitales_provincias2002 = (
+                transformer.get_departamentos_capitales_provincias_2002(
+                    self.spark, 
+                    self.df_silver_departamentos2002,
+                    self.df_silver_provincias2002
+                )
+            )
         except Exception as e:
             print(f"Error transforming data: {e}")
 
     def loading(self, create_database=False):
         try:
-            pass
+            loader = Load()
+             
+            loader.create_external_table_without_partition(
+                self.layer,
+                self.df_departamentos_capitales_provincias2002,
+                "departamentos_capitales_cantidad_provincias2002"
+            )
+            print("Table was loaded.")
         except Exception as e:
             print(f"Error loading data: {e}")
 
--- src/transformers/transformer.py
+++ src/transformers/transformer.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructType
+from pyspark.sql.functions import col, count
 from .config import PROJECT_NAME
 
 
@@ -148,3 +149,73 @@ class Transform():
             print(f"Error getting dataframe of categories: {e}")
             return None
 
+    def get_capitales_departamento_2002(self, spark: SparkSession, dataframe_1: DataFrame, dataframe_2: DataFrame):
+        """
+        Returns a dataframe with the department capitals of Peru
+        
+        Args:
+            spark (SparkSession): The SparkSession object.
+            dataframe_1 (DataFrame): The DataFrame object.
+            dataframe_2 (DataFrame): The DataFrame object.
+        """
+        try:
+            df_capitales_departamento2002 = (
+                dataframe_1.alias("d").join(dataframe_2.alias("p"), "coddpto")
+                .select(col("d.coddpto").alias("codigo_departamento"),
+                        col("d.nombre").alias("departamento"),
+                        col("p.nombre").alias("capital"))
+                .where("codprov = '01'")
+                .orderBy("codigo_departamento")
+            )
+    
+            return df_capitales_departamento2002
+        except Exception as e:
+            print(f"Error getting dataframe of the department capitals: {e}")
+            return None
+
+    def get_cantidad_provincias_departamento_2002(self, spark: SparkSession, dataframe_1: DataFrame, dataframe_2: DataFrame):
+        """
+        Returns a dataframe with the number of provinces per department of Peru
+        
+        Args:
+            spark (SparkSession): The SparkSession object.
+            dataframe_1 (DataFrame): The DataFrame object.
+            dataframe_2 (DataFrame): The DataFrame object.
+        """
+        try:
+            df_cantidad_provincias_departamento2002 = (
+                dataframe_1.alias("d").join(dataframe_2.alias("p"), "coddpto")
+                .groupby("d.coddpto")
+                .agg(count("p.codprov").alias("cantidad_provincias"))
+                .select(col("coddpto").alias("codigo_departamento"), "cantidad_provincias")
+                .orderBy("codigo_departamento")
+            )
+    
+            return df_cantidad_provincias_departamento2002
+        except Exception as e:
+            print(f"Error getting dataframe of the department capitals: {e}")
+            return None
+
+    def get_departamentos_capitales_provincias_2002(self, spark: SparkSession, dataframe_1: DataFrame, dataframe_2: DataFrame):
+        """
+        Returns a dataframe with the number of provinces per department of Peru
+        
+        Args:
+            spark (SparkSession): The SparkSession object.
+            dataframe_1 (DataFrame): The DataFrame object.
+            dataframe_2 (DataFrame): The DataFrame object.
+        """
+        try:
+            df_capitales_departamento2002 = self.get_capitales_departamento_2002(spark, dataframe_1, dataframe_2)
+            df_cantidad_provincias_departamento2002 = self.get_cantidad_provincias_departamento_2002(spark, dataframe_1, dataframe_2)
+            
+            df_departamentos_capitales_provincias2002 = (
+                df_capitales_departamento2002.join(df_cantidad_provincias_departamento2002, "codigo_departamento")
+                .select("*")
+                .orderBy("codigo_departamento")
+            )
+    
+            return df_departamentos_capitales_provincias2002
+        except Exception as e:
+            print(f"Error getting dataframe of the department capitals and provinces: {e}")
+            return None
